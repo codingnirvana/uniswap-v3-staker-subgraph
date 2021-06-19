35ca07d9b11c683ebccb716ad84c905846cdfa2f
shubham-kanodia <shubhamkanodia123@gmail.com>
2021-06-19 20:20:20 +0530

    Modifying mappings to save incentive entity

--- src/mappings/mapping.ts
+++ src/mappings/mapping.ts
@@ -7,49 +7,20 @@ import {
 import { Incentive } from "../types/schema"
 
 export function handleIncentiveCreated(event: IncentiveCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = Incentive.load(event.transaction.from.toHex())
+  let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new Incentive(event.transaction.from.toHex())
+  let entity = new Incentive(id);
+  
+  entity.pool = event.params.pool;
+  entity.rewardToken = event.params.rewardToken;
+  entity.refundee = event.params.refundee;
+  entity.isActive = true;
 
-    // Entity fields can be set using simple assignments
-    // entity.count = BigInt.fromI32(0)
-  }
+  entity.startTime = event.params.startTime;
+  entity.endTime = event.params.endTime;
+  entity.reward = event.params.reward;
 
-  // BigInt and BigDecimal math are supported
-  // entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  // entity.fee = event.params.fee
-  // entity.tickSpacing = event.params.tickSpacing
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.createPool(...)
-  // - contract.feeAmountTickSpacing(...)
-  // - contract.getPool(...)
-  // - contract.owner(...)
-  // - contract.parameters(...)
+  entity.save();
 }
 
 export function handleIncentiveEnded(event: IncentiveEnded): void {}
