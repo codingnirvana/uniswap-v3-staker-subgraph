ef7fdee1b501991d3b6625c33b53f729a088f906
shubham-kanodia <shubhamkanodia123@gmail.com>
2021-06-26 19:30:41 +0530

    Making changes for fixing host ip issue for linux

--- test/test.sh
+++ test/test.sh
@@ -5,11 +5,17 @@ DIRECTORY=$(cd `dirname $0` && pwd)
 __cleanup ()
 {
     docker-compose -f $DIRECTORY/docker-compose.yml stop
+
+    rm docker-compose.yml
+    mv docker-compose.yml-e docker-compose.yml
+
+    # Commenting this out due to failing pipeline
     rm -r -f $DIRECTORY/data
 }
 
 trap __cleanup EXIT
 
+./setup-host.sh
 docker-compose -f $DIRECTORY/docker-compose.yml up --build -d
 
 sleep 60
